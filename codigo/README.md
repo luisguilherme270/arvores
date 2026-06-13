ae7dd84d519a6ae89efb378653ba14409db426c3
Luis Guilherme Viana <luis.camargo2@estudante.ifms.edu.br>
2026-06-13 18:31:05 -0400

    Add files via upload

---
+++ codigo/README.md
@@ -0,0 +1,3 @@
+# Terminar apenas o main.c, se possível verificar tanto a biblioteca e o src:
+
+## Arquivos em respectiva ordem: main.c, main.h, src.c 
---
+++ codigo/apps/main.c
@@ -0,0 +1,14 @@
+/*
+    Falta só fazer o main.c
+    e revisar a biblioteca main.h, e o src.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int
+main()
+{
+    return 0;
+}
