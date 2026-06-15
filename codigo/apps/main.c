b7490073ed8744eae73f2c49de026121fc6e80e3
Luis Guilherme Viana <luis.camargo2@estudante.ifms.edu.br>
2026-06-15 10:24:54 -0400

    Add files via upload

--- codigo/apps/main.c
+++ codigo/apps/main.c
@@ -1,14 +1,41 @@
-/*
-    Falta só fazer o main.c
-    e revisar a biblioteca main.h, e o src.c
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
-int
-main()
-{
+struct No{
+    int dado;
+    struct No *esquerda;
+    struct No *direita;
+};
+
+int main(){
+
+    struct No* raiz = criarNo(1);
+    struct No* n1 = criarNo(2);
+    struct No* n2 = criarNo(3);
+    struct No* n3 = criarNo(4);
+    struct No* n4 = criarNo(5); 
+    
+    raiz->esquerda = n1;
+    raiz->direita = n2;
+
+    n1->esquerda = n3;
+    n2->direita = n4;
+
+    printf("pre ordem(raiz, esquerda, direita)\n");
+    preOrdem(raiz);
+    printf("\n\n");
+    
+     printf("em ordem( esquerda, raiz, direita)\n");
+    emOrdem(raiz);
+    printf("\n\n");
+
+    printf("pos ordem(esquerda, direita, raiz)\n");
+    posOrdem(raiz);
+    printf("\n\n");
+
+    desalocarNo(raiz);
+    if(raiz == NULL){ printf("deu certo"); }
     return 0;
+
 }
